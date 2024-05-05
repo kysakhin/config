60bbc0cf1794a9161bd9bbc040e14df47b34ed3d
kysakhin <akhinsreedalla1105@gmail.com>
2024-05-05 12:58:20 +0530

    5/5/24

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -x PATH /usr/condabin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/var/lib/snapd/snap/bin $PATH
+set -x PATH /home/akhin/anaconda3 $PATH
